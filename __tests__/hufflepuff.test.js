05c92e70e96b65e01cf8b7f919ba4f1a9af50173
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-11-10 12:50:40 -0800

    Hufflepuffs getAllHufflepuffs() correctly displaying / passing

---
+++ __tests__/hufflepuff.test.js
@@ -0,0 +1,44 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('Hufflepuffs routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('GET /hufflepuffs will return list of hufflepuffs', async () => {
+    const resp = await request(app).get('/hufflepuffs');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "firstName": "Cedric",
+          "id": "1",
+          "lastName": "Diggory",
+        },
+        Object {
+          "firstName": "Helga",
+          "id": "2",
+          "lastName": "Hufflepuff",
+        },
+        Object {
+          "firstName": "Newt",
+          "id": "3",
+          "lastName": "Scamander",
+        },
+        Object {
+          "firstName": "Pamona",
+          "id": "4",
+          "lastName": "Sprout",
+        },
+        Object {
+          "firstName": "Bridget",
+          "id": "5",
+          "lastName": "Wenlock",
+        },
+      ]
+    `);
+  });
+});
