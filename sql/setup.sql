0af78becf54e4e62df563ec6cec7d502d56135dd
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-11-09 18:00:55 -0800

    SQL with tables added

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,98 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS gryffindor;
+DROP TABLE IF EXISTS hufflepuff;
+DROP TABLE IF EXISTS ravenclaw;
+DROP TABLE IF EXISTS slytherin;
+DROP TABLE IF EXISTS death_eaters;
+
+
+CREATE TABLE gryffindor (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR
+);
+
+CREATE TABLE hufflepuff (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR
+);
+
+CREATE TABLE ravenclaw (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR
+);
+
+CREATE TABLE slytherin (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR
+);
+
+CREATE TABLE death_eaters (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR,
+    douchery_level BIGINT
+);
+
+INSERT INTO gryffindor (
+    first_name,
+    last_name
+)
+VALUES
+    ('Harry', 'Potter'),
+    ('Ron', 'Weasley'),
+    ('Hermione', 'Granger'),
+    ('Neville', 'Longbottom'),
+    ('Ginny', 'Weasley')
+    ;
+
+INSERT INTO hufflepuff (
+    first_name,
+    last_name
+)
+VALUES
+    ('Cedric', 'Diggory'),
+    ('Helga', 'Hufflepuff'),
+    ('Newt', 'Scamander'),
+    ('Pamona', 'Sprout'),
+    ('Bridget', 'Wenlock')
+    ;
+
+INSERT INTO ravenclaw (
+    first_name,
+    last_name
+)
+VALUES
+    ('Luna', 'Lovegood'),
+    ('Rowena', 'Ravenclaw'),
+    ('Garrick', 'Ollivander'),
+    ('Filius', 'Flitwick'),
+    ('Gilderoy', 'Lockhart')
+    ;
+
+INSERT INTO slytherin (
+    first_name,
+    last_name
+)
+VALUES
+    ('Draco', 'Malfoy'),
+    ('Salazar', 'Slytherin'),
+    ('Severus', 'Snape'),
+    ('Tom', 'Riddle'),
+    ('Horace', 'Slughorn')
+    ;
+
+INSERT INTO death_eaters (
+    name,
+    douchery_level
+)
+VALUES
+    ('Lord Voldemort', 100),
+    ('Bellatrix Lestrange', 74),
+    ('Lucius Malfoy', 52),
+    ('Pius Thicknesse', 10),
+    ('Stan Shunpike', 2)
+    ;
